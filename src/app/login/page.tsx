0e02416d1121fa1fa031b8264f6d6a598d2ab232
230-Luna <lsy77877@gmail.com>
2025-05-14 20:20:58 +0900

    login 페이지 작업중

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,18 +2,28 @@
 
 export default function LoginPage() {
   return (
-    <div style={{ margin: "80px auto", textAlign: "center" }}>
-      <h1>로그인</h1>
+    <div className="login-page-box">
+      <h1 className="login-h1">로그인</h1>
       <div className="join-box">
-        <h2>OSSCA 계정으로 로그인하기</h2>
+        <h2 className="login-h2">OSSCA 계정으로 로그인하기</h2>
         <form>
           <fieldset>
-            <ul className="input">
+            <ul>
               <li>
-                <input placeholder="이메일 주소 또는 아이디"></input>
+                <input
+                  type="text"
+                  title="아이디"
+                  className="input-style01"
+                  placeholder="이메일 주소 또는 아이디"
+                ></input>
               </li>
               <li>
-                <input placeholder="비밀번호"></input>
+                <input
+                  type="password"
+                  title="비밀번호"
+                  className="input-style01"
+                  placeholder="비밀번호"
+                ></input>
               </li>
             </ul>
             <ul className="checkbox">
